1eceb618d4108dafd2a88242ad3f7f6ba74f65e7
student <student@eda-bc-11.localdomain>
2022-12-01 11:43:24 +1300

    added  navbar compnonent

---
+++ client/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <>
+      <header className="header">
+        <h1>Nav bar</h1>
+      </header>
+    </>
+  )
+}
+
+export default NavBar
