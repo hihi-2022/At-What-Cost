ff86b8243fc60f3059cc464514f46414a1108f18
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-12-01 16:12:08 +1300

    add edit filter case to reducer and action creator

--- client/reducers/filtersReducer.js
+++ client/reducers/filtersReducer.js
@@ -1,4 +1,4 @@
-import { ADD_FILTER } from '../actions'
+import { ADD_FILTER, EDIT_FILTER } from '../actions'
 
 function filtersReducer(state = [], action) {
   const { type, payload } = action
@@ -6,6 +6,16 @@ function filtersReducer(state = [], action) {
   switch (type) {
     case ADD_FILTER:
       return [...state, payload]
+    case EDIT_FILTER: {
+      const { code: editCode } = payload
+      return [...state].map((item) => {
+        const { code } = item
+        if (code === editCode) {
+          return payload
+        }
+        return item
+      })
+    }
     default:
       return state
   }
