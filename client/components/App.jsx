ca9cf3236a54477682c946bec6a08107875dcb82
Louis Cowan <96268761+louiscowan@users.noreply.github.com>
2022-12-02 12:39:58 +1300

    welcome page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,7 +7,7 @@ import Welcome from './Welcome'
 function App() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar />
       <header className="header">
         <h1>App component: At what cost</h1>
       </header>
@@ -21,7 +21,8 @@ function App() {
             :
           <Welcome />
         }
-      </section>
+      </section> */}
+      <Welcome />
     </>
   )
 }
