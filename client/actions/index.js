c65bfcebb83d37200879683284f18660a5c0733f
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-12-01 15:21:39 +1300

    add categories reducer and get categories action creator

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,15 @@
 export const RECEIVE_TRANSACTIONS = 'RECEIVE_TRANSACTIONS'
+export const GET_CATEGORIES = 'GET_CATEGORIES'
 
 export function receiveTransactionsAction(transactions) {
   return {
     type: RECEIVE_TRANSACTIONS,
     payload: transactions,
+  }
+}
+
+export function getCategoriesAction() {
+  return {
+    type: GET_CATEGORIES,
   }
 }
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import transactionsListReducer from './transactionsListReducer'
+import categoriesReducer from './categoriesReducer'
 
 export default combineReducers({
   transactionsList: transactionsListReducer,
+  categories: categoriesReducer,
 })
