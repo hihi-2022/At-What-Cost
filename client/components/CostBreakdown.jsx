1860488028c7f49da3e531eae0a2d5cb941d39ab
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-12-02 14:29:57 +1300

    add styles to various components and amend jsx to suit styling

--- client/components/CostBreakdown.jsx
+++ client/components/CostBreakdown.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import CostSummary from './CostSummary'
 import GraphSummary from './GraphSummary'
 
+import style from '../styles/CostBreakdown.module.scss'
+
 function CostBreakdown({ totals }) {
   return (
-    <div>
+    <div className={style.container}>
       <h2>The CostBreakdown component</h2>
       <GraphSummary totals={totals} />
       <CostSummary totals={totals} />
--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,55 +1,61 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import Papa from "papaparse"
+import Papa from 'papaparse'
+import style from '../styles/NavBar.module.scss'
 
-import {receiveTransactionsAction} from '../actions'
+import { receiveTransactionsAction } from '../actions'
 
-const allowedExtensions = ["csv"]
- 
-function NavBar () {  
+const allowedExtensions = ['csv']
 
-  const [error, setError] = useState("")
-  const [file, setFile] = useState("")
+function NavBar() {
+  const [error, setError] = useState('')
+  const [file, setFile] = useState('')
 
   const dispatch = useDispatch()
 
   const handleFileChange = (e) => {
-    setError("")
-      
+    setError('')
+
     if (e.target.files.length) {
       const inputFile = e.target.files[0]
-          
-      const fileExtension = inputFile.type.split("/")[1]
-    if (!allowedExtensions.includes(fileExtension)) {
-      setError("Please input a csv file")
-      return
-    }
-    setFile(inputFile)
+
+      const fileExtension = inputFile.type.split('/')[1]
+      if (!allowedExtensions.includes(fileExtension)) {
+        setError('Please input a csv file')
+        return
+      }
+      setFile(inputFile)
     }
   }
-    
-  const handleParse = () => {
-        
-    if (!file) return alert("Please enter a CSV file")
+
+  const handleParse = (e) => {
+    e.preventDefault()
+    if (!file) return alert('Please enter a CSV file')
 
     const reader = new FileReader()
     reader.readAsText(file)
-    
+
     reader.onload = async ({ target }) => {
       const csv = Papa.parse(target.result, { header: true })
       const parsedData = csv.data
-      const filteredData = parsedData.map(obj => {
-        return {Amount: obj.Amount, Date: obj.Date, Code: obj.Code, Type: obj.Type }
+      const filteredData = parsedData.map((obj) => {
+        return {
+          Amount: obj.Amount,
+          Date: obj.Date,
+          Code: obj.Code,
+          Type: obj.Type,
         }
-      )
-    dispatch(receiveTransactionsAction(filteredData))
+      })
+      dispatch(receiveTransactionsAction(filteredData))
     }
   }
- 
-    return (
-      <div>
-        <div style={{fontFamily: 'Nova Round, cursive', fontSize: '45px', fontWeight: 'bold'}}>AWC</div>
-          <label htmlFor="csvInput" style={{ display: "block" }}>
+
+  return (
+    <nav className={style.nav}>
+      <div className={style.container}>
+        <h1>AWC</h1>
+        <form onSubmit={handleParse}>
+          <label htmlFor="csvInput" style={{ display: 'block' }}>
             Enter CSV File
           </label>
           <input
@@ -58,10 +64,10 @@ function NavBar () {
             name="file"
             type="File"
           />
-          <div>
-            <button onClick={handleParse}>Submit</button>
-          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-    )
-  }
+    </nav>
+  )
+}
 export default NavBar
