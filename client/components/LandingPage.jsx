1affc0bd9c0ecb0e36355d747e2f9b8d228336dd
moo-kusay <112349689+moo-kusay@users.noreply.github.com>
2022-12-03 16:00:44 +1300

    landing page with login to take to dashboard

---
+++ client/components/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import style from '../styles/welcome.module.css'
+
+function Welcome() {
+  return (
+    <div className={style.page}>
+      <img className={style.welcomePageImage} src="/assets/welcomePageImage.jpg" alt="money" />
+      <h2>AT WHAT COST!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque optio aperiam, alias voluptatibus assumenda recusandae praesentium rerum qui repellat debitis eos ipsa distinctio fugiat iste amet quibusdam ea et. Exercitationem voluptatibus minima asperiores dolores beatae quod iusto, id dolorem voluptates veritatis fugiat maiores recusandae soluta. Ab recusandae, deserunt similique rerum tempore maiores commodi praesentium impedit, nostrum aliquam dignissimos velit? Labore cum unde, eius quae exercitationem ipsa facere velit. Sunt magnam quae ipsa exercitationem alias. Accusamus animi maiores quod eius explicabo fugiat facilis. Perferendis quis error, eveniet qui quae sunt ipsum reprehenderit. Ut, distinctio est, dignissimos amet quod eveniet, atque sint sunt ad ullam quo esse vitae recusandae rerum quaerat sapiente! At deleniti omnis voluptatem a? Tenetur praesentium laudantium, vel atque, commodi eveniet asperiores eos consequuntur officia excepturi dicta, reiciendis facilis quam quis magnam repellat impedit inventore. Cum beatae quibusdam ullam, quas nemo ut labore aperiam dolore eveniet incidunt cupiditate earum, mollitia accusamus natus! Explicabo, placeat labore in architecto optio ullam 
+      </p>
+    </div>
+  )
+  
+}
+
+export default Welcome
---
+++ client/components/UploadFile.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import Papa from 'papaparse'
+import style from '../styles/NavBar.module.scss'
+
+import { receiveTransactionsAction } from '../actions'
+
+const allowedExtensions = ['csv']
+
+function NavBar() {
+  const [error, setError] = useState('')
+  const [file, setFile] = useState('')
+
+  const dispatch = useDispatch()
+
+  const handleFileChange = (e) => {
+    setError('')
+
+    if (e.target.files.length) {
+      const inputFile = e.target.files[0]
+
+      const fileExtension = inputFile.type.split('/')[1]
+      if (!allowedExtensions.includes(fileExtension)) {
+        setError('Please input a csv file')
+        return
+      }
+      setFile(inputFile)
+    }
+  }
+
+  const handleParse = (e) => {
+    e.preventDefault()
+    if (!file) return alert('Please enter a CSV file')
+
+    const reader = new FileReader()
+    reader.readAsText(file)
+
+    reader.onload = async ({ target }) => {
+      const csv = Papa.parse(target.result, { header: true })
+      const parsedData = csv.data
+      const filteredData = parsedData.map((obj) => {
+        return {
+          amount: Number(obj.Amount),
+          date: obj.Date,
+          code: obj.Code,
+          type: obj.Type,
+          category: ""
+        }
+      })
+      dispatch(receiveTransactionsAction(filteredData))
+    }
+  }
+
+  return (
+    <nav className={style.nav}>
+      <div className={style.container}>
+        <h1>AWC</h1>
+        <form onSubmit={handleParse}>
+          <label htmlFor="csvInput" style={{ display: 'block' }}>
+            Enter CSV File
+          </label>
+          <input
+            onChange={handleFileChange}
+            id="csvInput"
+            name="file"
+            type="File"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </nav>
+  )
+}
+export default NavBar
