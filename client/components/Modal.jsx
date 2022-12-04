14de8578aef0ab7ed130cacf3c0ee6dd9ef8e6ee
moo-kusay <112349689+moo-kusay@users.noreply.github.com>
2022-12-05 10:31:46 +1300

    code display clean up

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -32,7 +32,7 @@ function Modal() {
     <div className={style.container}>
       <div className={style.modal}>
         <h1>{isAdd ? 'Add' : 'Edit'}</h1>
-        <h2>Code</h2>
+        <h2>{code}</h2>
         <form onSubmit={handleSubmit} className={style.category_form}>
           <div className={style.select_control}>
             <label htmlFor="category">Choose Category:</label>
