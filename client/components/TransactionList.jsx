3f59586cc9537c02235e4df358c136c33303ee90
Aaron Armour <111632207+aaron-armour-hihi-22@users.noreply.github.com>
2022-12-05 18:27:40 +1300

    Add radio buttons to choose selection in TransactionList (but styling broken now)

--- client/components/TransactionList.jsx
+++ client/components/TransactionList.jsx
@@ -5,6 +5,7 @@ import style from '../styles/TransactionList.module.scss'
 
 function TransactionList() {
   const [data, setData] = useState()
+  const [selection, setSelection] = useState('all')
 
   const transactionData = useSelector(
     (globalState) => globalState.transactionsList
@@ -16,12 +17,26 @@ function TransactionList() {
     setData(transactionData)
   }, [transactionData])
 
+  const handleSelectionChange = (e) => {
+    setSelection(e.target.value)
+  }
+
   return (
-    <ul className={style.list}>
-      {data?.map((transactionData, index) => {
-        return <TransactionItem transactionData={transactionData} colours={colours} key={index} />
-      })}
-    </ul>
+    <div>
+      <ul className={style.list}>
+        {data?.map((transactionData, index) => {
+          return <TransactionItem transactionData={transactionData} colours={colours} key={index} />
+        })}
+      </ul>
+      <form onChange={handleSelectionChange}>
+        <input type="radio" id='all' name='all' value='all' checked={selection === 'all'} />
+        <label htmlFor='all'> all </label>
+        <input type="radio" id='filtered' name='filtered' value='filtered' checked={selection === 'filtered'} />
+        <label htmlFor='filtered'> filtered </label>
+        <input type="radio" id='unfiltered' name='unfiltered' value='unfiltered' checked={selection === 'unfiltered'} />
+        <label htmlFor='unfiltered'> unfiltered </label>
+      </form>
+    </div>
   )
 }
 
