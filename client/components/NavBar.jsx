a46ec93836715a712cb79132cb57ff1c15937d46
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-12-06 12:50:25 +1300

    add get user filters dispatch to navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -3,15 +3,18 @@ import style from '../styles/NavBar.module.scss'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { app } from '../../firebase'
 import TheButton from './TheButton'
+import { useDispatch } from 'react-redux'
+import { receieveUserFiltersThunk } from '../actions'
 
 function NavBar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   const auth = getAuth(app)
-
+  const dispatch = useDispatch()
   onAuthStateChanged(auth, (user) => {
     if (user) {
       setIsLoggedIn(true)
+      dispatch(receieveUserFiltersThunk(user.uid))
     } else {
       setIsLoggedIn(false)
     }
