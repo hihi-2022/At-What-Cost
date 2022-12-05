d6c3e5b146c824391eb0d25f9f7c5e72d4f02dae
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-12-05 16:41:52 +1300

    navigate to home page from sign up page if already logged in

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,14 +1,28 @@
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
-import React, { useState } from 'react'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth'
+import React, { useState, useEffect } from 'react'
 import style from '../styles/SignUp.module.scss'
 import { app } from '../../firebase'
+import { useNavigate } from 'react-router-dom'
 
 function SignUp() {
   const auth = getAuth(app)
+  const navigate = useNavigate()
   const [userDetails, setUserDetails] = useState({
     userEmail: '',
     userPassword: '',
   })
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        return navigate('/')
+      }
+    })
+  }, [])
 
   const { userEmail: email, userPassword: password } = userDetails
 
