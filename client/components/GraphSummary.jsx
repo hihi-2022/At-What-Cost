bf2f1a13594d8828ccda9c034389e5de56970d2b
Aaron Armour <111632207+aaron-armour-hihi-22@users.noreply.github.com>
2022-12-07 09:26:45 +1300

    Set up CostBreakdown and GraphSummary for new chart to switch to

--- client/components/GraphSummary.jsx
+++ client/components/GraphSummary.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import { ResponsiveContainer, PieChart, Pie, Tooltip, Legend } from 'recharts'
 
-function GraphSummary({ expenseTotals }) {
-  return (
-    <div style={{ width: '500px', height: '300px' }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
-          <Pie
-            dataKey="amount"
-            nameKey="category"
-            isAnimationActive={false}
-            data={expenseTotals}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-          />
-          <Legend layout='vertical' align='right' verticalAlign='middle'/>
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
-    </div>
-  )
+function GraphSummary({ incomeTotal, expenseTotals }) {
+  if (true) {
+    return (
+      <div style={{ width: '500px', height: '300px' }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart width={400} height={400}>
+            <Pie
+              dataKey="amount"
+              nameKey="category"
+              isAnimationActive={false}
+              data={expenseTotals}
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+            />
+            <Legend layout='vertical' align='right' verticalAlign='middle' />
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    )
+  }
 }
 
 export default GraphSummary
