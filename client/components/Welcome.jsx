2542e40fbe9070c4331cc87d45ce278ab4cf87c5
moo-kusay <112349689+moo-kusay@users.noreply.github.com>
2022-12-06 17:05:04 +1300

    created doseOfWisdom.js with Aaron and random quotes features

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import style from '../styles/Welcome.module.scss'
+import { doseOfWisdom } from "../doseOfWisdom";
 
 function Welcome() {
+  const [wisdom, setWisdom] = useState({})
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * doseOfWisdom.length)
+    setWisdom(doseOfWisdom[randomIndex])
+    const intervalId = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * doseOfWisdom.length)
+      setWisdom(doseOfWisdom[randomIndex])
+    }, 5000)
+    
+    return () => clearInterval(intervalId)
+  }, [])
+ 
   return (
     <div className={style.page}>
       <img className={style.welcomePageImage} src="/assets/welcomePageImage.jpg" alt="money" />
@@ -12,6 +26,7 @@ function Welcome() {
         It's so easy to use, just go to the upload button and give us a csv file from your bank and watch the magic happen.
         If you want us to hold your saved categories, just sign up and we'll do it for you.
       </p>
+      <p>{wisdom.quote} - {wisdom.author}</p>
     </div>
   )
   
