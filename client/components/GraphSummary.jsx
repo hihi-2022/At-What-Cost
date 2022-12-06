7f5518c99918b5b484c182f3f194ba1417ac59d0
Aaron Armour <111632207+aaron-armour-hihi-22@users.noreply.github.com>
2022-12-07 08:25:58 +1300

    Separate out income and transfer from expenses

--- client/components/GraphSummary.jsx
+++ client/components/GraphSummary.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ResponsiveContainer, PieChart, Pie, Tooltip, Legend } from 'recharts'
 
-function GraphSummary({ totals }) {
+function GraphSummary({ expenseTotals }) {
   return (
     <div style={{ width: '500px', height: '300px' }}>
       <ResponsiveContainer width="100%" height="100%">
@@ -10,7 +10,7 @@ function GraphSummary({ totals }) {
             dataKey="amount"
             nameKey="category"
             isAnimationActive={false}
-            data={totals}
+            data={expenseTotals}
             cx="50%"
             cy="50%"
             outerRadius={80}
