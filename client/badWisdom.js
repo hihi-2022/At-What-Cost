4e93871489671ef966bb85bffaf1c7ee35c1e5b1
Louis Cowan <96268761+louiscowan@users.noreply.github.com>
2022-12-07 15:16:59 +1300

    adding wisdom

--- client/badWisdom.js
+++ client/badWisdom.js
@@ -11,6 +11,18 @@ const badWisdom = [
     quote: "Store physical money under your mattress(you can't trust banks)",
     author: "Alyssa Barbieri"
   },
+  {
+    quote: "Time = Money. Time = Construct. Therefore Money is a construct ",
+    author: "Louis Cowan"
+  },
+  {
+    quote: "Gambling is fun",
+    author: "Joseph"
+  },
+  {
+    quote: "It's always a possibility that you win the lotto",
+    author: "Moo"
+  },
 ]
 
 module.exports = { badWisdom }
