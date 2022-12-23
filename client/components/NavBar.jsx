5e24aefc6ee49e7e1d6a7ffad3f8f4cfc8f0815b
Louis Cowan <louiscowan7@gmail.com>
2022-12-23 16:07:39 +1300

    completed downloadable csv file

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -251,48 +251,33 @@ function NavBar() {
     auth.signOut()
   }
 
-  // download csv
   const download = function (data) {
  
-    // Creating a Blob for having a csv file format
-    // and passing the data with type
     const blob = new Blob([data], { type: 'text/csv' });
  
-    // Creating an object for downloading url
     const url = window.URL.createObjectURL(blob)
- 
-    // Creating an anchor(a) tag of HTML
+
     const a = document.createElement('a')
  
-    // Passing the blob downloading url
     a.setAttribute('href', url)
  
-    // Setting the anchor tag attribute for downloading
-    // and passing the download file name
     a.setAttribute('download', 'download.csv');
- 
-    // Performing a download with click
+
     a.click()
 }
  
 const csvmaker = function (data) {
  
-    // Empty array for storing the values
     const csvRows = [];
     
     const headers = Object.keys(data[0]);
  
-    // As for making csv format, headers
-    // must be separated by comma and
-    // pushing it into array
     csvRows.push(headers.join(','));
  
-    // Pushing Object values into array
-    // with comma separation
-    const values = Object.values(data.map(object => {return object})).join(',');
-    csvRows.push(values)
+    data.forEach((item) => {
+      csvRows.push(Object.values(item).join(','))
+    })
  
-    // Returning the array joining with new line
     return csvRows.join('\n')
 }
  
