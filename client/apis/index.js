f1cca78eea14ea6feaa77bac06bfbfc348f08571
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-01-12 19:49:02 +1300

    Changes to now store both filters and custom categories in Firebase for a logged in user

--- client/apis/index.js
+++ client/apis/index.js
@@ -17,12 +17,24 @@ export async function updateUserFiltersAPI(uuid, filters) {
   const db = getFirestore(app)
   const docRef = doc(db, 'users', uuid)
   const updatedFilters = { filters }
-  await setDoc(docRef, updatedFilters)
+  await setDoc(docRef, updatedFilters, { merge: true })
+}
+
+export async function getUserCustomCategoriesAPI(uuid) {
+  const db = getFirestore(app)
+
+  const docRef = doc(db, 'users', uuid)
+  const querySnapshot = await getDoc(docRef)
+  if (querySnapshot.exists()) {
+    const { customCategories } = querySnapshot.data()
+    return customCategories
+  }
+  return null
 }
 
 export async function updateCustomCategoriesAPI(uuid, customCategories) {
   const db = getFirestore(app)
   const docRef = doc(db, 'users', uuid)
   const updatedCustomCategories = { customCategories }
-  await setDoc(docRef, updatedCustomCategories)
+  await setDoc(docRef, updatedCustomCategories, { merge: true })
 }
