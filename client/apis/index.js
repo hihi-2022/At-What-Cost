7eb795958a4caeee49ad71cd607b3cbbd391bce4
Aaron Armour <aaron.armour.hihi.22@gmail.com>
2023-01-12 15:41:42 +1300

    Add a function to update custom categories in Firebase

--- client/apis/index.js
+++ client/apis/index.js
@@ -1,5 +1,6 @@
 import { app } from '../../firebase'
 import { getFirestore, getDoc, setDoc, doc } from 'firebase/firestore'
+import { async } from '@firebase/util'
 
 export async function getUserFiltersAPI(uuid) {
   const db = getFirestore(app)
@@ -18,4 +19,11 @@ export async function updateUserFiltersAPI(uuid, filters) {
   const docRef = doc(db, 'users', uuid)
   const updatedFilters = { filters }
   await setDoc(docRef, updatedFilters)
+}
+
+export async function updateCustomCategoriesAPI(uuid, customCategories) {
+  const db = getFirestore(app)
+  const docRef = doc(db, 'users', uuid)
+  const updatedCustomCategories = { customCategories }
+  await setDoc(docRef, updatedCustomCategories)
 }
